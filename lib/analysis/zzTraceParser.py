d8ce26abeddffe377f87580f19c485d9a66d2b48
Wensheng <wsdou55@gmail.com>
2019-08-15 22:40:32 +0800

    Add aync IOs.

--- lib/analysis/zzTraceParser.py
+++ lib/analysis/zzTraceParser.py
@@ -13,7 +13,7 @@ import TraceParser
 class Trace:
     def __init__(self):
 		self.events = list()
-		self.libuvIOEvents = list()
+		self.ioActions = list()
 		pass
 
     def length(self):
@@ -28,6 +28,7 @@ class Event:
     def __init__(self):
 		self.id = -1
         self.priority = -1
+		self.resolve = None
         self.ops = list()
         pass
 
@@ -44,8 +45,9 @@ class Event:
     def getEnd(self):
         return self.get(self.length())
 
-class LibuvIOEvent:
+class IOAction:
     def __init__(self):
+		self.registerOp = None
         self.fileAccessOP = None
         self.resolveOp = None
         pass
@@ -58,7 +60,6 @@ class RegisterOp:
         self.resourceType = resourceType
         pass
 
-
 class ResolveOp:
     def __init__(self, lineno, prior, follower, resourceType):
         self.lineno = lineno
@@ -67,7 +68,7 @@ class ResolveOp:
         self.resourceType = resourceType
         pass
 
-class DataAccessOP:
+class DataAccessOp:
     def __init__(self, lineno, entryType, accessType, accessVar):
         self.lineno = lineno
         self.entryType = entryType
@@ -76,12 +77,14 @@ class DataAccessOP:
         pass
 
 class FileAccessOp (object):
-    def __init__(self, lineno, entryType, accessType, resource, ref, name, isAsync):
+    def __init__(self, lineno, entryType, accessType, resource, isAsync):
         self.lineno = lineno
         self.entryType = entryType
         self.accessType = accessType
         self.accessFile = resource
         self.isAsync = isAsync
+		self.register = None
+		self.resolve = None
         pass
 
 def processTraceFile(traceFile):
@@ -93,6 +96,8 @@ def processTraceFile(traceFile):
 	variables = result['vars']
 	files = result['files']
 
+	lineNo2Ops = dict()
+
     for testcase in testsuit.values():
 		trace = Trace()
 
@@ -103,6 +108,7 @@ def processTraceFile(traceFile):
 			event = Event()
 			event.id = cb.asyncId
 			event.priority = getPriority(cb.resourceType)
+			event.resolve = lineNo2Ops[cb.resolve]
 			trace.events.append(event)
 
 			if len(cb.records) == 0:
@@ -110,15 +116,17 @@ def processTraceFile(traceFile):
 
 			rcdList = cb.records
 			for j in range(0, len(rcdList) - 1):
-				record = cb.records[j]
+				record = records[j]
 				if isinstance(record, TraceParser.DataAccessRecord):
-					daOp = DataAccessOP()
+					daOp = DataAccessOp()
 					event.ops.append(daOp)
 
 					daOp.lineno = record.lineno
 					daOp.entryType = record.entryType
 					daOp.accessType = record.accessType
 					daOp.accessVar = record.getId()
+
+					lineNo2Ops[record.lineno] = daOp
 
 				elif isinstance(record, TraceParser.FileAccessRecord):
 					faOp = FileAccessOp()
@@ -129,6 +137,21 @@ def processTraceFile(traceFile):
 					faOp.accessType = record.accessType
 					faOp.accessFile = record.resource
 					faOp.isAsync = record.isAsync
+
+					lineNo2Ops[record.lineno] = faOp
+
+					#Assume: the registration and resolve operation has been parsed.
+					if (faOp.isAsync):
+						resOp = lineNo2Ops[record.resolve]
+						regOp = lineNo2Ops[record.resolve[:-1]]
+						event.ops.remove(resOp)
+						event.ops.remove(faOp)
+
+						ioAction = IOAction()
+						ioAction.registerOp = regOp
+						ioAction.fileAccessOP = faOp
+						ioAction.resolveOp = resOp
+						trace.ioActions.append(ioAction)
 
 				elif isinstance(record, TraceParser.Reg_or_Resolve_Op):
 					rLineno = record.lineno
@@ -141,6 +164,8 @@ def processTraceFile(traceFile):
 						resolveOp.follower = record.follower
 						resolveOp.resourceType = record.resourceType
 
+						lineNo2Ops[record.lineno] = resolveOp
+
 					else: # registration operation
 						registerOp = RegisterOp()
 						event.ops.append(registerOp)
@@ -149,6 +174,8 @@ def processTraceFile(traceFile):
 						registerOp.prior = record.prior
 						registerOp.follower = record.follower
 						registerOp.resourceType = record.resourceType
+
+						lineNo2Ops[record.lineno] = registerOp
 
     exit
     return result
